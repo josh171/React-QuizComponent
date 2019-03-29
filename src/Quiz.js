fc5678cb6ace17f42700338bce0486b5bc3350a8
Joshua Delany-Booth <boothjos@IOM5009554.local>
2019-03-29 14:51:12 +0000

    Commit

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,12 +12,12 @@ class Quiz extends Component {
     }
   };
   render() {
-  const isQuizEnd = ((this.state.quiz_position.position -1) ===
+  const isQuizEnd = ((this.state.quiz_position -1) ===
   quizData.quiz_questions.length)
     return (
         <div>
-          <QuizEnd />
-          <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]}/>
+          {isQuizEnd ? <QuizEnd /> :
+          <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]}/>}
         </div>
     );
   }
